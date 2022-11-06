ae79955b1b2dc7aa5fe468f35f36a97409f39fd9
OneZerocococo <ylflyfly@gmail.com>
2022-11-06 15:33:00 +0800

    feat: add mongoose and dotenv

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const PORT = 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 app.use(routes)
-
+require('./config/mongoose')
 
 app.listen(PORT, () => {
   console.log(`Express is runnnig on http://localhost:${PORT}`)
