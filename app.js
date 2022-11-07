a7385b5c8f986131ae6c99f50ee224911309e0f4
OneZerocococo <ylflyfly@gmail.com>
2022-11-07 09:18:02 +0800

    feat: add express hbs and index page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
+const exphbs = require('express-handlebars')
 const PORT = 3000
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
 app.use(routes)
 require('./config/mongoose')
 
