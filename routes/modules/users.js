0adb1364b67bfad45214034210ab23e574f8f7e4
OneZerocococo <ylflyfly@gmail.com>
2022-11-07 20:08:57 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,5 +15,11 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.get('/logout', (req, res, next) => {
+  req.logout(err => {
+    if (err) { return next(err) }
+    res.redirect('/users/login')
+  })
+})
 
 module.exports = router
