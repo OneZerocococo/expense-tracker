e39873262b384d4cd1e05cb3117b6333d6a24403
OneZerocococo <ylflyfly@gmail.com>
2022-11-08 19:25:12 +0800

    feat: add message partials

--- config/passport.js
+++ config/passport.js
@@ -4,14 +4,14 @@ const User = require('../models/user')
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: '此帳號尚未註冊過!' })
+          return done(null, false, req.flash('warning_msg', '此帳號尚未註冊過!'))
         }
         if (user.password !== password) {
-          return done(null, false, { message: '帳號或密碼錯誤!' })
+          return done(null, false, req.flash('warning_msg', '帳號或密碼錯誤!'))
         }
         return done(null, user)
       })
