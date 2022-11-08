4e6f00fee9f909b49ca04860e130935b33ab8797
OneZerocococo <ylflyfly@gmail.com>
2022-11-08 15:33:02 +0800

    feat: add create expense function

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
 
 router.get('/new', (req, res) => {
   res.render('new')
 })
-router.get('/:id/edit', (req, res) => {
+
+router.post('/', (req, res) => {
+  const userId = req.user._id
+  req.body.userId = userId
+  Record.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
+router.get('/edit', (req, res) => {
   res.render('edit')
 })
 
