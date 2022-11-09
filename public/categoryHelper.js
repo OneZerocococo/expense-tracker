47afc3c1823e759aa63f04d217d1cf4d7f81280b
OneZerocococo <ylflyfly@gmail.com>
2022-11-09 12:28:12 +0800

    feat: add index categorySelector function

--- public/categoryHelper.js
+++ public/categoryHelper.js
@@ -2,9 +2,7 @@ const handlebars = require('handlebars')
 const Category = require('../models/category')
 
 const showIcon = handlebars.registerHelper('showIcon', function (num) {
-  console.log('helper running..')
   Category.findOne({ id: num }, (err, category) => {
-    console.log(category.icon)
     return category.icon
   })
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,17 +1,51 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  let totalAmount = 0
-  Record.find({ userId })
+  const categoryId = Number(req.query.categorySelector)
+  const categories = []
+  // 先render所有類別的花費，若有切換selector再依照類別顯示...
+  Category.find()
     .lean()
-    .then(expenses => {
-      expenses.forEach(expense => {
-        totalAmount += expense.amount
-      })
-      res.render('index', { expenses, totalAmount })
+    .then(category => categories.push(...category))
+    .then(() => {
+      if (!categoryId) {
+        Record.find({ userId })
+          .lean()
+          .sort({ date: 'desc' })
+          .then(expenses => {
+            let totalAmount = 0
+            categories.forEach(category => {
+              if (category.id === categoryId) {
+                category.selected = true
+              }
+            })
+            expenses.forEach(expense => {
+              totalAmount += expense.amount
+            })
+            return res.render('index', { categories, expenses, totalAmount })
+          })
+      } else {
+        // specific category
+        Record.find({ userId, category: categoryId })
+          .lean()
+          .sort({ date: 'desc' })
+          .then(expenses => {
+            let totalAmount = 0
+            categories.forEach(category => {
+              if (category.id === categoryId) {
+                category.selected = true
+              }
+            })
+            expenses.forEach(expense => {
+              totalAmount += expense.amount
+            })
+            return res.render('index', { categoryId, categories, expenses, totalAmount })
+          })
+      }
     })
     .catch((error) => console.log(error))
 })
