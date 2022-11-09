82a4c0a42817f3dc2e6a8e63980981900d9fd465
OneZerocococo <ylflyfly@gmail.com>
2022-11-09 18:42:35 +0800

    modify variable name of recordSeeder and add home image

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,14 +1,14 @@
 const bcrypt = require('bcryptjs')
 const Record = require('../record')
 const User = require('../user')
-const recordList = require('../seeds/record.json').results
-const userList = require('../seeds/user.json').results
+const SEED_RECORD = require('../seeds/record.json').results
+const SEED_USER = require('../seeds/user.json').results
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
   console.log('Start writing user and record seeds...')
   Promise.all(
-    userList.map(user => {
+    SEED_USER.map(user => {
       const { name, email, password, recordIndex } = user
       return User.create({
         name,
@@ -17,7 +17,7 @@ db.once('open', () => {
       })
         .then(user => {
           const records = recordIndex.map(index => {
-            const record = recordList[index]
+            const record = SEED_RECORD[index]
             record.userId = user._id
             return record
           })
