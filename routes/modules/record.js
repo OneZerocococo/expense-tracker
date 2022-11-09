213711681025777c434285aa032da7693158f30c
OneZerocococo <ylflyfly@gmail.com>
2022-11-09 15:30:51 +0800

    modify categorySelector for new page

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -4,7 +4,12 @@ const Record = require('../../models/record')
 const Category = require('../../models/category')
 
 router.get('/new', (req, res) => {
-  res.render('new')
+  Category.find()
+    .lean()
+    .then(categories => {
+      res.render('new', { categories })
+    })
+    .catch(err => console.log(err))
 })
 
 router.post('/', (req, res) => {
